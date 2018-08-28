fb5d4d9db407ba00fbc41ff40154fd4463839117
liyuepeng <wamk08@sina.com>
2018-08-28 16:02:18 +0800

    这个

---
+++ src/components/base/swiper/swiper.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="swiper">
+        <mt-swipe :auto="3000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+            <mt-swipe-item>4</mt-swipe-item>
+        </mt-swipe>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+.swiper {
+    height: 320px;
+    border: 1px solid red;
+}
+</style>
+
--- src/components/business/index/index.vue
+++ src/components/business/index/index.vue
@@ -1,9 +1,25 @@
 <template>
-    
+    <div class="index-content">
+        <index-header></index-header>
+        <swiper></swiper>
+        <mfooter></mfooter>
+    </div>
 </template>
 <script>
+import IndexHeader from 'business/indexHeader/indexHeader'
+import Swiper from 'base/swiper/swiper'
+import Mfooter from 'business/mFooter/mFooter'
 export default {
-    
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        IndexHeader,
+        Swiper,
+        Mfooter
+    }
 }
 </script>
 <style lang="less" scoped>
