1f60d49f17dc21e6c3ac9736f66810bc3c73530c
liyuepeng <wamk08@sina.com>
2018-08-28 13:08:05 +0800

    11

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from 'business/index/index'
 import Information from 'business/information/Information'
-
 Vue.use(Router)
 
 export default new Router({
