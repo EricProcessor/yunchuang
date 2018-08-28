aa1a44492232e761115dfeb970629e563c8accb8
liyuepeng <wamk08@sina.com>
2018-08-28 12:05:39 +0800

    UI框架替换成mint-ui

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import MintUI from 'mint-ui'
 import '../static/css/reset.css'
+import 'mint-ui/lib/style.min.css'
 Vue.config.productionTip = false
 
-Vue.use(ElementUI);
+Vue.use(MintUI);
 
 /* eslint-disable no-new */
 new Vue({
