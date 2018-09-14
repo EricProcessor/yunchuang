a010efa5ec7b018d42ef4f6d5e7f522632011939
zhucongcong <15110253002@163.com>
2018-09-14 17:51:48 +0800

    调试认证导师

--- src/components/business/setTeacher/setTeacher.vue
+++ src/components/business/setTeacher/setTeacher.vue
@@ -21,7 +21,7 @@
           <p class="sexMan fl" v-if="sex.maleActive">
             <img src="./man_2.png" alt="">
           </p>
-          <input type="radio" v-model="setTeacher.sex" value="男" @click="maleChange" class="maleRadio" :checked="sex.maleActive">
+          <input type="radio" value="男" @click="maleChange" class="maleRadio" :checked="sex.maleActive">
           <span class="fl">男</span>
         </div>
         <div class="fl sexBox pos1" :class="{sexManActive:sex.femaleActive}">
@@ -31,14 +31,14 @@
           <p class="sexMan fl" v-if="sex.femaleActive">
             <img src="./woman_2.png" alt="" />
           </p>
-          <input type="radio" v-model="setTeacher.sex" value="女" @click="femaleChange" class="maleRadio" :checked="sex.femaleActive">
+          <input type="radio" value="女" @click="femaleChange" class="maleRadio" :checked="sex.femaleActive">
           <span class="fl">女</span>
         </div>
       </li>
       <li class="group clearfix">
         <label class="fl pos1">
           <i v-if="formList.isShow">*</i>出生日期</label>
-        <input class="fl" v-model="setTeacher.birthday" type="text" />
+        <span @click.prevent="_openPicker"><input class="fl" disabled v-model="setTeacher.birthday" type="text" placeholder="点击选择" /></span>
       </li>
       <li class="group clearfix">
         <label class="fl pos1">
@@ -58,7 +58,7 @@
             <img src="./agree.png" alt="" />
           </i>
           <i class="fl" v-if="!invitation.agree"></i>
-          <input type="radio" v-model="setTeacher.invitaer" value="接受" class="agreeBtn" @click="agreeInviter" :checked="invitation.agree">
+          <input type="radio" value="接受" class="agreeBtn" @click="agreeInviter" :checked="invitation.agree">
           <span class="fl">接受</span>
         </div>
         <div class="fl clearfix similarRadio pos1">
@@ -66,7 +66,7 @@
             <img src="./agree.png" alt="" />
           </i>
           <i class="fl" v-if="!invitation.disagree"></i>
-          <input type="radio" v-model="setTeacher.invitaer" value="不接受" class="agreeBtn" @click="disagreeInviter" :checked="invitation.disagree">
+          <input type="radio" value="不接受" class="agreeBtn" @click="disagreeInviter" :checked="invitation.disagree">
           <span class="fl">不接受</span>
         </div>
       </li>
@@ -74,17 +74,15 @@
         <label class="fl pos1">
           <i v-if="formList.isShow">*</i>{{selectName.carrier}}</label>
         <select class="fl" v-model="setTeacher.serviceCarrier">
-          <option value="0">金融服务</option>
-          <option value="1">投资服务</option>
-          <option value="2">融资服务</option>
+          <option v-for="(v,i) in this.selectName.carrierArr" :key="i" :value="v.fswId">{{v.fswName}}</option>
+
         </select>
       </li>
       <li class="group clearfix">
         <label class="fl pos1">
           <i v-if="formList.isShow">*</i>{{selectName.unit}}</label>
         <select class="fl" v-model="setTeacher.unitAttrbute">
-          <option value="0">国有</option>
-          <option value="1">民营</option>
+          <option v-for="(v,i) in selectName.unitArr" :key="i" :value="v.fswId">{{v.fswName}}</option>
         </select>
       </li>
       <li class="group clearfix">
@@ -122,16 +120,14 @@
         <label class="fl pos1">
           <i v-if="!formList.isShow">*</i>{{selectName.tutorship}}</label>
         <select class="fl" v-model="setTeacher.speciality">
-          <option value="0">国有</option>
-          <option value="1">民营</option>
+          <option v-for="(v,i) in this.selectName.torshipArr" :key="i" :value="v.fswId">{{v.fswName}}</option>
         </select>
       </li>
       <li class="group clearfix">
         <label class="fl pos1">
           <i v-if="formList.isShow">*</i>{{selectName.technicalField}}</label>
         <select class="fl" v-model="setTeacher.technicalField">
-          <option value="0">国有</option>
-          <option value="1">民营</option>
+          <option v-for="(v,i) in selectName.technicalFieldArr" :key="i" :value="v.fswId">{{v.fswName}}</option>
         </select>
       </li>
       <li class="group clearfix pos1">
@@ -161,14 +157,18 @@
     </ol>
     <ul class="clearfix operatBtn">
       <li class="fl" @click="resetBtn">重置</li>
-      <li class="fl preserve">保存</li>
+      <li class="fl preserve" @click="conserveBtn">保存</li>
     </ul>
+    <!-- 日期picker -->
+    <mt-datetime-picker ref="datePicker" type="date" @confirm="_handleTime" :startDate="new Date('1970-01-01')"></mt-datetime-picker>
+    <!-- 地址 -->
     <AddressPick @selectAddress="getAddr" ref="addr"></AddressPick>
   </div>
 </template>
 <script>
 import IndexHeader from "business/indexHeader/indexHeader";
 import AddressPick from "base/addressPick/addressPick";
+import Install from "@/config/checkRule";
 export default {
   data() {
     return {
@@ -195,19 +195,25 @@ export default {
         carrier: "服务载体",
         unit: "单位属性",
         tutorship: "辅导特长",
-        technicalField:"技术领域"
+        technicalField: "技术领域",
+        carrierType: "载体类型",
+        torship: "辅助特长",
+        carrierArr: [],
+        unitArr: [],
+        torshipArr: [],
+        technicalFieldArr: []
       },
       //整体数据
       setTeacher: {
         name: "", //姓名
-        sex: "", //性别
+        sex: "男", //性别
         birthday: "", //出生日期
         company: "", //工作单位
         presentPost: "", //现任职务
-        inviter: "", //是否接受邀请
+        inviter: "接受", //是否接受邀请
         serviceCarrier: "", //服务载体
         unitAttrbute: "", //单位属性
-        serviceArea: "", //服务区域
+        serviceArea: "", //服务区域-详细地址
         zipCode: "", //邮编
         phone: "", //手机
         email: "", //邮箱
@@ -230,14 +236,20 @@ export default {
   },
   mounted() {
     this.getServiceCarrier();
+    this.getUnitAttribute();
+    this.getTutorship();
+    this.getTechnicalField();
   },
   methods: {
+    //唤出地址-城市信息
     showAddr() {
       this.$refs.addr.showAddressComponent();
     },
+    //获得地址-城市信息
     getAddr(val) {
       this.address = val;
     },
+    //关闭顶部提示
     closeBtn() {
       this.formList.isClose = !this.formList.isClose;
     },
@@ -245,59 +257,169 @@ export default {
       if (!this.sex.maleActive) {
         this.sex.maleActive = !this.sex.maleActive;
         this.sex.femaleActive = !this.sex.femaleActive;
+        this.setTeacher.sex = "男";
       }
     },
     femaleChange() {
       if (!this.sex.femaleActive) {
         this.sex.femaleActive = !this.sex.femaleActive;
         this.sex.maleActive = !this.sex.maleActive;
+        this.setTeacher.sex = "女";
       }
     },
     agreeInviter() {
       if (!this.invitation.agree) {
         this.invitation.agree = !this.invitation.agree;
         this.invitation.disagree = !this.invitation.disagree;
+        this.setTeacher.inviter = "接受";
       }
     },
     disagreeInviter() {
       if (!this.invitation.disagree) {
         this.invitation.disagree = !this.invitation.disagree;
         this.invitation.agree = !this.invitation.agree;
+        this.setTeacher.inviter = "不接受";
       }
+    },
+    //打开时间选择器
+    _openPicker() {
+      this.$refs.datePicker.open();
+    },
+    //转换得到的时间格式
+    _handleTime(date) {
+      let year = date.getFullYear(),
+        month = date.getMonth() - 0 + 1,
+        day = date.getDate();
+      this.setTeacher.birthday = `${year}-${month}-${day}`;
     },
     //获取服务载体
     getServiceCarrier() {
-      let _url="/fronttutorauthenticationselect-home";
-      this.axios.post(_url,{name:this.selectName.carrier}).then(res=>{
-        console.log(res);
+      let _url = "/fronttutorauthenticationselect-home";
+      this.axios.post(_url, { name: this.selectName.carrierType }).then(res => {
+        this.selectName.carrierArr = res.data;
       });
     },
     //获取单位类型
     getUnitAttribute() {
-      let _url="/fronttutorauthenticationselect-home";
-      this.axios.post(_url,{name:this.selectName.unit}).then(res=>{
-        console.log(res);
+      let _url = "/fronttutorauthenticationselect-home";
+      this.axios.post(_url, { name: this.selectName.unit }).then(res => {
+        this.selectName.unitArr = res.data;
       });
     },
     //获取辅导特长
     getTutorship() {
-      let _url="/fronttutorauthenticationselect-home";
-      this.axios.post(_url,{name:this.selectName.tutorship}).then(res=>{
-        console.log(res);
+      let _url = "/fronttutorauthenticationselect-home";
+      this.axios.post(_url, { name: this.selectName.torship }).then(res => {
+        this.selectName.torshipArr = res.data;
       });
     },
     //获取技术领域
     getTechnicalField() {
-      let _url="/fronttutorauthenticationselect-home";
-      this.axios.post(_url,{name:this.selectName.technicalField}).then(res=>{
-        console.log(res);
-      });
+      let _url = "/fronttutorauthenticationselect-home";
+      this.axios
+        .post(_url, { name: this.selectName.technicalField })
+        .then(res => {
+          this.selectName.technicalFieldArr = res.data;
+        });
     },
-    //清空所有信息
+    //重置所有信息
     resetBtn() {
       for (let key in this.setTeacher) {
         if (this.setTeacher[key] != "") {
           this.setTeacher[key] = "";
+        }
+      }
+    },
+    //保存信息
+    conserveBtn() {
+      //信息验证
+      for (let key in this.setTeacher) {
+        if (key == "name" && this.setTeacher[key] == "") {
+          alert("姓名不能为空");
+          return;
+        } else if (key == "birthday" && this.setTeacher[key] == "") {
+          alert("出生日期不能为空！");
+          return;
+        } else if (key == "company" && this.setTeacher[key] == "") {
+          alert("工作单位不能为空！");
+          return;
+        } else if (key == "serviceCarrier" && this.setTeacher[key] == "") {
+          alert("服务载体不能为空！");
+          return;
+        } else if (key == "unitAttrbute" && this.setTeacher[key] == "") {
+          alert("单位属性不能为空！");
+          return;
+        } else if (
+          key == "serviceArea" &&
+          this.setTeacher[key] == "" &&
+          this.address == ""
+        ) {
+          alert("服务区域不能为空！");
+          return;
+        } else if (key == "phone") {
+          if (this.setTeacher[key] == "") {
+            alert("手机号不能为空");
+          } else {
+            if (!Install.isPhone(this.setTeacher[key])) {
+              alert("手机号码格式不正确！");
+            }
+          }
+          return;
+        } else if (key == "eamil") {
+          if (this.setTeacher[key] == "") {
+            alert("邮箱不能为空");
+          } else {
+            if (!Install.isEmail(this.setTeacher[key])) {
+              alert("邮箱格式不正确！");
+            }
+          }
+          return;
+        } else if (key == "positionalTitles" && this.setTeacher[key] == "") {
+          alert("职称不能为空！");
+          return;
+        } else if (key == "highestEducation" && this.setTeacher[key] == "") {
+          alert("最高学历不能为空！");
+          return;
+        } else if (key == "goodTheme" && this.setTeacher[key] == "") {
+          alert("技术领域不能为空！");
+          return;
+        } else if (key == "resume" && this.setTeacher[key] == "") {
+          alert("个人简历不能为空！");
+          return;
+        } else {
+          console.log(key, this.setTeacher[key]);
+          let _serviceUrl = "/fronttutorauthenticationsave-home";
+          let set_addr = this.address + " " + this.setTeacher.serviceArea;
+          let datas = {
+            fciName: this.setTeacher.name, //名字
+            fciSex: this.setTeacher.sex, //性别
+            fciBirthday: this.setTeacher.birthday, //生日
+            fciOrgName: this.setTeacher.company, //工作单位
+            fciPosition: this.setTeacher.presentPost, //职务
+            fciFlag: this.setTeacher.inviter, //是否接收邀请
+            fcciId: this.setTeacher.serviceCarrier, //服务载体
+            fciAttribute: this.setTeacher.unitAttrbute, //单位属性
+            fciAddress: set_addr, //地址
+            fciPostcode: this.setTeacher.zipCode, //邮编
+            fciTel: this.setTeacher.phone, //手机
+            fciMile: this.setTeacher.email, //邮箱
+            fciTechnical: this.setTeacher.positionalTitles, //职称
+            fciEducation: this.setTeacher.highestEducation, //学历
+            fciPoint: this.setTeacher.speciality, //辅导特长
+            fciDomain: this.setTeacher.technicalField, //技术领域
+            fciTheme: this.setTeacher.goodThememe, //擅长主题
+            fciResume: this.setTeacher.resume, //个人简历
+            fciEnterprise: this.setTeacher.coachEnterprise, //辅导企业
+            fciAppointment: this.setTeacher.employment, //受聘情况
+            fciLatitude: this.setTeacher.fciLatitude, //
+            fciLongitude: this.setTeacher.fciLongitude //
+            // provinceId: this.setTeacher.name,  //省
+            //cityId: this.setTeacher.name, //市
+            // areaId : this.setTeacher.name //区
+          };
+          this.axios.post(_serviceUrl,datas).then(res => {
+            console.log(res);
+          });
         }
       }
     }
