c52f16dc0d80068cf95cef1c245c1683b2a8b1ce
yuanna0323 <15010611339@163.com>
2018-09-19 18:55:55 +0800

    (yuanna)

--- src/components/business/classroom/classroom.vue
+++ src/components/business/classroom/classroom.vue
@@ -16,68 +16,68 @@
     </div>
 </template>
 <script>
-import IndexHeader from 'business/indexHeader/indexHeader'
-import MFooter from 'business/mFooter/mFooter'
-import ClassTab from 'business/classTab/classTab'
-import Swiper from 'base/swiper/swiper'
+import IndexHeader from "business/indexHeader/indexHeader";
+import MFooter from "business/mFooter/mFooter";
+import ClassTab from "business/classTab/classTab";
+import Swiper from "base/swiper/swiper";
 
 export default {
-    components: {
-        IndexHeader,
-        MFooter,
-        Swiper,
-        ClassTab
-    }
-}
+  components: {
+    IndexHeader,
+    MFooter,
+    Swiper,
+    ClassTab
+  }
+};
 </script>
 <style lang="less" scoped>
-.classroom{
-    .video-box {
-        height: 370px;
-        background: #cad3dc;
-        position: relative;
-        .is-live {
-            background: red;
-            width: 180px;
-            height: 50px;
-            position: absolute;
-            top: 0;
-            left: 0;
-            text-align: center;
-            line-height: 50px;
-            color: #fff;
-            font-size: 20px;
-        }
-        .play-butt {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            margin-left: -60px;
-            margin-top: -90px;
-            width: 120px;
-            height: 120px;
-            img {
-                width: 120px;
-                height: 120px;
-            }
-        }
-        .state {
-            width: 100%;
-            height: 60px;
-            box-sizing: border-box;
-            background: rgba(0, 0, 0, 0.6);
-            line-height: 60px;
-            padding-left: 30px;
-            color: #fff;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            font-size: 24px;
-            white-space: nowrap;
-            text-overflow: ellipsis;
-            overflow: hidden;
-        }
+.classroom {
+  .video-box {
+    height: 370px;
+    background: #cad3dc;
+    position: relative;
+    .is-live {
+      background: red;
+      width: 180px;
+      height: 50px;
+      position: absolute;
+      top: 0;
+      left: 0;
+      text-align: center;
+      line-height: 50px;
+      color: #fff;
+      font-size: 20px;
     }
+    .play-butt {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-left: -60px;
+      margin-top: -90px;
+      width: 120px;
+      height: 120px;
+      img {
+        width: 120px;
+        height: 120px;
+      }
+    }
+    .state {
+      width: 100%;
+      height: 60px;
+      box-sizing: border-box;
+      background: rgba(0, 0, 0, 0.6);
+      line-height: 60px;
+      padding-left: 30px;
+      color: #fff;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      font-size: 24px;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+  }
 }
 </style>
 
